654661845a5618ff3b7054627f03b5afd3753e16
root <root@srvapp.minous.lan>
2022-10-31 23:26:05 +0100

    nextcloud WIP: move to nginx and postgresql

--- bash/config/nextcloud.sh
+++ bash/config/nextcloud.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-description="Deploy nextcloud server"
+description="Deploy nextcloud server (nginx + postgresql)"
 # version: 0.1
 # author: Choops <choopsbd@gmail.com>
 
@@ -136,7 +136,11 @@ sys_config(){
 ask_nc_user(){
     read -rp "Nextcloud user ? " nc_user
     [[ ! ${nc_user} ]] && echo -e "${ERR} Need a username" && ask_nc_user
-    echo
+}
+
+ask_nc_mail(){
+    read -rp "Nextcloud user's email ? " nc_mail
+    [[ ${nc_user} != *@*;* ]] && echo -e "${ERR} Need a email address" && ask_nc_mail
 }
 
 ask_nc_pass(){
@@ -144,30 +148,29 @@ ask_nc_pass(){
 
     read -rp "Nextcloud user's password ? " nc_pass
     [[ ! ${nc_pass} ]] && echo -e "${ERR} Need a password" && ask_nc_pass
-    echo
 
     # TODO: test password strength
 
     read -rp "Confirm Nextcloud user's password: " nc_pass2
     [[ ${nc_pass2} ]] && echo
 
-    [[ "${nc_pass2}" != "${nc_pass}" ]] &&
-        echo -e "${ERR} Different from defined password" && ask_nc_pass
-
-    echo
+    [[ "${nc_pass2}" != "${nc_pass}" ]] && echo -e "${ERR} Different password" && ask_nc_pass
 }
 
 configure_database(){
-    echo -e "${INFO} Configuring database..."
+    echo -e "${NFO} Configuring database..."
 
-    mysql_secure_installation
-
-    ask_nc_user
+    ask_nc_mail
     ask_nc_pass
 
-    mysql -u root -p -e "create database nextcloud;"
-    mysql -u root -p -e "create user '${nc_user}'@'%' identified by '${nc_pass}';"
-    mysql -u root -p -e "grant all privileges on nextcloud.* to '${nc_user}'@'%';"
+    echo -e "${NFO} Commands to pass to postgresql:"
+    echo -e "${YLO}  CREATE USER nextcloud WITH PASSWORD '${nc_pass}';${DEF}"
+    echo -e "${YLO}  CREATE DATABASE nextcloud TEMPLATE template0 ENCODING 'UNICODE';${DEF}"
+    echo -e "${YLO}  ALTER DATABASE nextcloud OWNER TO nextcloud;${DEF}"
+    echo -e "${YLO}  GRANT ALL PRIVILEGES ON DATABASE nextcloud TO nextcloud;${DEF}"
+    echo -e "${YLO}  \\q${DEF}"
+
+    postgres psql
 }
 
 nginx_nextcloud_conf(){
@@ -179,19 +182,21 @@ server {
     root /var/www/nextcloud;
     index index.php;
 
-    location / {
-        try_files \$uri \$uri/ /index.php?\$args;
-    }
+    # Add headers to serve security related headers
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Robots-Tag none;
+    add_header X-Download-Options noopen;
+    add_header X-Permitted-Cross-Domain-Policies none;
+    add_header Referrer-Policy no-referrer;
 
-    location = /favicon.ico {
-        log_not_found off;
-        access_log off;
-    }
+    add_header X-Frame-Options "SAMEORIGIN";
 
-    location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
-        expires max;
-        log_not_found off;
-    }
+    # Path to the root of Nextcloud installation
+    root /usr/share/nginx/nextcloud/;
+
+    access_log /var/log/nginx/nextcloud.access;
+    error_log /var/log/nginx/nextcloud.error;
 
     location = /robots.txt {
         allow all;
@@ -199,34 +204,127 @@ server {
         access_log off;
     }
 
-    location ~ \.php$ {
-        include /etc/nginx/fastcgi_params;
-        fastcgi_pass unix:/run/php/php-fpm.sock;
-        fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+    # The following 2 rules are only needed for the user_webfinger app.
+    # Uncomment it if you're planning to use this app.
+    #rewrite ^/.well-known/host-meta /public.php?service=host-meta last;
+    #rewrite ^/.well-known/host-meta.json /public.php?service=host-meta-json
+    # last;
+
+    location = /.well-known/carddav {
+        return 301 \$scheme://\$host/remote.php/dav;
+    }
+    location = /.well-known/caldav {
+       return 301 \$scheme://\$host/remote.php/dav;
+   }
+
+    location ~ /.well-known/acme-challenge {
+      allow all;
+  }
+
+    # set max upload size
+    client_max_body_size 512M;
+    fastcgi_buffers 64 4K;
+
+    # Disable gzip to avoid the removal of the ETag header
+    gzip off;
+
+    # Uncomment if your server is build with the ngx_pagespeed module
+    # This module is currently not supported.
+    #pagespeed off;
+
+    error_page 403 /core/templates/403.php;
+    error_page 404 /core/templates/404.php;
+
+    location / {
+       rewrite ^ /index.php;
+   }
+
+    location ~ ^/(?:build|tests|config|lib|3rdparty|templates|data)/ {
+       deny all;
+   }
+    location ~ ^/(?:\.|autotest|occ|issue|indie|db_|console) {
+       deny all;
+   }
+
+    location ~ ^/(?:index|remote|public|cron|core/ajax/update|status|ocs/v[12]|updater/.+|ocs-provider/.+|core/templates/40[34])\.php(?:$|/) {
+       include fastcgi_params;
+       fastcgi_split_path_info ^(.+\.php)(/.*)$;
+       try_files \$fastcgi_script_name =404;
+       fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+       fastcgi_param PATH_INFO \$fastcgi_path_info;
+       #Avoid sending the security headers twice
+       fastcgi_param modHeadersAvailable true;
+       fastcgi_param front_controller_active true;
+       fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+       fastcgi_intercept_errors on;
+       fastcgi_request_buffering off;
+   }
+
+    location ~ ^/(?:updater|ocs-provider)(?:$|/) {
+       try_files \$uri/ =404;
+       index index.php;
+   }
+
+    # Adding the cache control header for js and css files
+    # Make sure it is BELOW the PHP block
+    location ~* \.(?:css|js)$ {
+        try_files \$uri /index.php\$uri\$is_args\$args;
+        add_header Cache-Control "public, max-age=7200";
+        # Add headers to serve security related headers (It is intended to
+        # have those duplicated to the ones above)
+        add_header X-Content-Type-Options nosniff;
+        add_header X-XSS-Protection "1; mode=block";
+        add_header X-Robots-Tag none;
+        add_header X-Download-Options noopen;
+        add_header X-Permitted-Cross-Domain-Policies none;
+        add_header Referrer-Policy no-referrer;
+        # Optional: Don't log access to assets
+        access_log off;
+    }
+
+   location ~* \.(?:svg|gif|png|html|ttf|woff|ico|jpg|jpeg)$ {
+        try_files \$uri /index.php\$uri\$is_args\$args;
+        # Optional: Don't log access to other assets
+        access_log off;
     }
 }
 EOF
 }
 
-install_nextcloud(){
-    systemctl enable mariadb
-    systemctl enable nginx
+secure_nextcloud(){
+    cat <<EOF >> /etc/nginx/conf.d/nextcloud.conf
+add_header Strict-Transport-Security "max-age=31536000" always;
+listen 443 ssl http2; # managed by Certbot
+EOF
+}
 
+install_nextcloud(){
     configure_database
 
     wget https://download.nextcloud.com/server/releases/latest.zip -P /tmp
-    
-    pushd /tmp
-    7z x latest.zip
-    cp -r nextcloud /var/www/
-    popd >/dev/null
-    chmod -R 755 /var/www/nextcloud
 
-    mkdir -p /var/nextcloud/data
-    chmod -R 755 /var/nextcloud
+    7z x /tmp/latest.zip -o/usr/share/nginx/
+
+    mkdir -p /usr/share/nginx/nextcloud
+    chown -R www-data:www-data /usr/share/nginx/nextcloud
+    #chmod -R 755 /usr/share/nginx/nextcloud
+
+    mkdir -p /usr/share/nginx/nextcloud-data
+    chown -R www-data:www-data /usr/share/nginx/nextcloud-data
+    #chmod -R 755 /usr/share/nginx/nextcloud-data
 
     nginx_nextcloud_conf
+
+    nginx -t
+
+    systemctl enable nginx
+    systemctl enable --now php7.4-fpm
+
+    certbot --nginx --agree-tos --redirect --staple-ocsp --email "${nc_mail}" -d "$(hostname -f)"
+
+    secure_nextcloud
+
+    # TODO: additional configuration
 
     systemctl restart php7.4-fpm nginx
 
